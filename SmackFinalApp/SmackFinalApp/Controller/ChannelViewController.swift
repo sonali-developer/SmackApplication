22628b66540931a3fc1489b6a7ee0a9f5e059023
SONALI PATEL <patelsonali123@yahoo.com>
2018-02-01 22:46:42 -0500

    Finished doing the Channel View Desgin

---
+++ SmackFinalApp/SmackFinalApp/Controller/ChannelViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChannelViewController.swift
+//  SmackFinalApp
+//
+//  Created by Sonali Patel on 1/30/18.
+//  Copyright © 2018 Sonali Patel. All rights reserved.
+//
+
+import UIKit
+
+class ChannelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+
+        
+    }
+    
+
+   
+
+}
---
+++ SmackFinalApp/SmackFinalApp/Controller/ChatViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ChatViewController.swift
+//  SmackFinalApp
+//
+//  Created by Sonali Patel on 1/30/18.
+//  Copyright © 2018 Sonali Patel. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(revealViewController().tapGestureRecognizer())
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
