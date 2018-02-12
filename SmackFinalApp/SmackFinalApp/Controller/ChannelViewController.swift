9c2020ea0ee79b76632fbf3d84343f4757442b06
SONALI PATEL <patelsonali123@yahoo.com>
2018-02-12 00:48:33 -0500

    Successfully finished designing returnUIColor in Channel VC and login profile setup

--- SmackFinalApp/SmackFinalApp/Controller/ChannelViewController.swift
+++ SmackFinalApp/SmackFinalApp/Controller/ChannelViewController.swift
@@ -14,9 +14,13 @@ class ChannelViewController: UIViewController {
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue:UIStoryboardSegue) {}
     
+    @IBOutlet weak var userImage: CircleImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelViewController.userDataDidChange(_notif:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        
 
         
     }
@@ -25,6 +29,18 @@ class ChannelViewController: UIViewController {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
+    @objc func userDataDidChange(_notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+        loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+        userImage.image = UIImage(named: UserDataService.instance.avatarName)
+        userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        }
+        else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
+    }
    
 
 }
---
+++ SmackFinalApp/SmackFinalApp/Controller/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  SmackFinalApp
+//
+//  Created by Sonali Patel on 2/12/18.
+//  Copyright © 2018 Sonali Patel. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+   
+}
